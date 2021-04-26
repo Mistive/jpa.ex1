7eb1f9a7018e83dfe85197e88b3fd8661a7427f8
mistive <pms9725@gmail.com>
2021-04-26 20:50:18 +0900

    다대다[N:N]

--- src/main/java/jpa/ex1/Member.java
+++ src/main/java/jpa/ex1/Member.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -22,6 +24,10 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    //다대다
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public void setUserMember(String name){
         this.name = name;
---
+++ src/main/java/jpa/ex1/Product.java
@@ -0,0 +1,20 @@
+package jpa.ex1;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "product")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
+}
